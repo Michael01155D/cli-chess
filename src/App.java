45e9b46ab4286e7f807f174c58ae5aa115797743
Michael Mroz <michaelmroz01111@gmail.com>
2024-06-25 13:09:38 -0300

    add todo comment into App for next thing to work on

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ public class App {
         GameBoard gameBoard = new GameBoard();
         gameBoard.displayBoard();
         Piece pawn = gameBoard.getPiece(1, 0);
+        //current todo: make sure pawn's validMoves are valid, then test w other pawns
         gameBoard.seeValidMoves(pawn);
     }
 }
