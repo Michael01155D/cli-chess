3d63cc05245ad483b67ab3f49b19266fe8bf6313
Michael Mroz <michaelmroz01111@gmail.com>
2024-07-09 18:02:54 -0300

    begin implementing valid move search for knights

--- src/Knight.java
+++ src/Knight.java
@@ -12,6 +12,39 @@ public class Knight extends Piece {
 
     public void findValidMoves(Piece[][] boardState) {
         ArrayList<Integer[]> moves = new ArrayList<>();
+        /*
+        knight can move (row, col): 
+        - (+2, + 1)
+        - (+2, -1)
+        - (+1, +2)
+        - (+1, -2)
+        - (-2, + 1)
+        - (-2, -1)
+        - (-1, +2)
+        - (-1, -2)
+        */
+        int currRow = getPosition()[0];
+        int currCol = getPosition()[1];
+        
+        if (currRow < boardState.length -2 && currCol < boardState.length - 1) {
+            moves.add(new Integer[] {currRow + 2, currCol + 1}); 
+        }
+        if (currRow < boardState.length - 1 && currCol < boardState.length - 2) {
+            moves.add(new Integer[] {currRow + 1, currCol + 2});
+        }
+        if (currRow < boardState.length - 2 && currCol > 0) {
+            moves.add(new Integer[] {currRow +2, currCol -1});
+        }
+        if (currRow < boardState.length -1 && currCol > 1) {
+            moves.add(new Integer[] {currRow + 1, currCol - 2});
+        }
+        if (currRow > 1 && currCol < boardState.length -1) {
+            moves.add(new Integer[] {currRow - 2, currCol + 1});
+        }
+        if (currRow > 0 && currCol < boardState.length - 2) {
+
+        }
+
         setValidMoves(moves);
     }
 
