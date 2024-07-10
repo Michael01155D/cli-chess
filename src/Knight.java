12ecb8d31d1f0dd0ad133de3d5e09379910026c0
Michael Mroz <michaelmroz01111@gmail.com>
2024-07-09 21:05:06 -0300

    implemented knight valid move check. requires testing and improve the code to be less repetitive

--- src/Knight.java
+++ src/Knight.java
@@ -26,28 +26,38 @@ public class Knight extends Piece {
         int currRow = getPosition()[0];
         int currCol = getPosition()[1];
         
-        if (currRow < boardState.length -2 && currCol < boardState.length - 1) {
+        if (currRow < boardState.length -2 && currCol < boardState.length - 1 && isAvailable(currRow + 2, currCol + 1, boardState)) {
             moves.add(new Integer[] {currRow + 2, currCol + 1}); 
         }
-        if (currRow < boardState.length - 1 && currCol < boardState.length - 2) {
+        if (currRow < boardState.length - 1 && currCol < boardState.length - 2  && isAvailable(currRow + 1, currCol + 2, boardState)) {
             moves.add(new Integer[] {currRow + 1, currCol + 2});
         }
-        if (currRow < boardState.length - 2 && currCol > 0) {
+        if (currRow < boardState.length - 2 && currCol > 0  && isAvailable(currRow + 2, currCol - 1, boardState)) {
             moves.add(new Integer[] {currRow +2, currCol -1});
         }
-        if (currRow < boardState.length -1 && currCol > 1) {
+        if (currRow < boardState.length -1 && currCol > 1  && isAvailable(currRow + 1, currCol -2, boardState)) {
             moves.add(new Integer[] {currRow + 1, currCol - 2});
         }
-        if (currRow > 1 && currCol < boardState.length -1) {
+        if (currRow > 1 && currCol < boardState.length -1  && isAvailable(currRow - 2, currCol + 1, boardState)) {
             moves.add(new Integer[] {currRow - 2, currCol + 1});
         }
-        if (currRow > 0 && currCol < boardState.length - 2) {
-
+        if (currRow > 0 && currCol < boardState.length - 2  && isAvailable(currRow - 1, currCol + 2, boardState)) {
+            moves.add(new Integer[] {currRow - 1, currCol + 2});
+        }
+        if (currRow > 1 && currCol > 0  && isAvailable(currRow - 2, currCol - 1, boardState)) {
+            moves.add(new Integer[] {currRow - 2, currCol - 1});
+        }
+        if (currRow > 0 && currCol > 1  && isAvailable(currRow - 1, currCol - 2, boardState)) {
+            moves.add(new Integer[] {currRow - 1, currCol - 2});
         }
 
         setValidMoves(moves);
     }
 
+    //helper fn to check if legal board space is also valid. ie: is empty or occupied by enemy piece
+    private boolean isAvailable(int row, int col, Piece[][] board) {
+        return board[row][col] == null || !(board[row][col].getColor().equals(this.getColor()));
+    }
     public String toString() {
         return "knight";
     }
