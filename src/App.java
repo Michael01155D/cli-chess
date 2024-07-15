4a43858b5039fede212c277bfd46248ac047e72c
Michael Mroz <michaelmroz01111@gmail.com>
2024-07-15 17:53:18 -0300

    tested rook movement

--- src/App.java
+++ src/App.java
@@ -3,8 +3,10 @@ public class App {
         GameBoard gameBoard = new GameBoard();
         //for testing: gameboard.addTestPiece(Piece) -> Piece foo = gameboard.getPiece(row, col) -> gameBoard.seeValidMoves(foo);
         Piece rook = gameBoard.getPiece(7, 0);
-        gameBoard.addTestPiece(new Rook("black", new int[] {4, 4}));
+        Rook testRook = new Rook("black", new int[] {4, 4});
+        gameBoard.addTestPiece(testRook);
         gameBoard.displayBoard();
         gameBoard.seeValidMoves(gameBoard.getPiece(4, 4));
+        gameBoard.movePiece(testRook);
     }
 }
