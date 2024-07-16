d3685db6f235e998e1da217f3696f63c1a5c3a98
Michael Mroz <michaelmroz01111@gmail.com>
2024-07-16 12:06:55 -0300

    begin adding bishop movement logic

--- src/App.java
+++ src/App.java
@@ -2,11 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         GameBoard gameBoard = new GameBoard();
         //for testing: gameboard.addTestPiece(Piece) -> Piece foo = gameboard.getPiece(row, col) -> gameBoard.seeValidMoves(foo);
-        Piece rook = gameBoard.getPiece(7, 0);
-        Rook testRook = new Rook("black", new int[] {4, 4});
-        gameBoard.addTestPiece(testRook);
+        Bishop testBishop = new Bishop("black", new int[] {4, 4});
+        gameBoard.addTestPiece(testBishop);
         gameBoard.displayBoard();
         gameBoard.seeValidMoves(gameBoard.getPiece(4, 4));
-        gameBoard.movePiece(testRook);
     }
 }
--- src/Bishop.java
+++ src/Bishop.java
@@ -9,7 +9,62 @@ public class Bishop extends Piece {
 
     public void findValidMoves(Piece[][] boardState) {
         ArrayList<Integer[]> moves = new ArrayList<>();
+        //same logic as rook, while next space is valid and not occupied, add to moves, if occupied add to moves if opposite color piece
+        String[] dirs = {"upLeft", "upRight", "downLeft", "downRight"};
+        for (String dir: dirs) {
+            addMoves(dir, boardState, moves);
+        }
         setValidMoves(moves);
+    }
+
+    //helper function to find valid moves given the bishop's direction
+    public void addMoves(String dir, Piece[][] boardState, ArrayList<Integer[]> moves) {
+        int currRow = this.getPosition()[0];
+        int currCol = this.getPosition()[1];
+        boolean incrementNextRow = dir.equals("downLeft") || dir.equals("downRight");
+        boolean incrementNextCol = dir.equals("upRight") || dir.equals("downRight");
+        int nextRow = incrementNextRow ? currRow + 1 : currRow - 1;
+        int nextCol = incrementNextCol ? currCol + 1 : currCol - 1;
+        
+        boolean notAtEdge = checkNextPosition(dir, nextRow, nextCol);
+        
+        while (notAtEdge) {
+            //if the next board position in current direction isnt empty, stop iterating. add position if piece is opposite color.
+            if (boardState[nextRow][nextCol] != null) {
+                if (!boardState[nextRow][nextCol].getColor().equals(this.getColor())) {
+                    moves.add(new Integer[] {nextRow, nextCol});
+                }
+                break;
+            }
+            //if next board position empty, add it to moves and update nextRow/nextCol and check if reached edge of board. 
+            moves.add(new Integer[] {nextRow, nextCol});
+            nextRow = incrementNextRow ? nextRow + 1 : nextRow - 1;
+            nextCol = incrementNextCol ? nextCol + 1 : nextCol - 1;
+            notAtEdge = checkNextPosition(dir, nextRow, nextCol);  
+        }
+    }
+
+    //helper function called by addMoves to update the while loop's condition value after each movement
+    private boolean checkNextPosition(String dir, int nextRow, int nextCol) {
+        boolean notAtEdge;
+        switch (dir) {
+            case "upLeft":
+                notAtEdge = nextRow > -1 && nextCol > -1;
+                break;
+            case "upRight":
+                notAtEdge = nextRow > -1 && nextCol < 8;
+                break;
+            case "downLeft":
+                notAtEdge = nextRow < 8 && nextCol > -1;
+                break;
+            case "downRight":
+                notAtEdge = nextRow < 8 && nextCol < 8;
+                break;
+            default:
+                notAtEdge = false;
+                break;
+        }
+        return notAtEdge;
     }
 
     public String toString() {
